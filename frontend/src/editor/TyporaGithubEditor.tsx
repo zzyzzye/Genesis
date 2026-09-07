f40653d3779efd0f6b8a657bb9cca4a21acadfde
zzyzzye <1511995259@qq.com>
2026-09-07 15:50:37 +0800

    拆分 Typora GitHub 编辑主题

---
+++ frontend/src/editor/TyporaGithubEditor.tsx
@@ -0,0 +1,198 @@
+import {
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  CodeToggle,
+  CreateLink,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+  headingsPlugin,
+  imagePlugin,
+  InsertCodeBlock,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  ListsToggle,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin,
+  Separator,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  type Translation,
+  UndoRedo,
+} from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+
+import './typora-github.css'
+
+const mdxEditorChineseText: Record<string, string> = {
+  "admonitions.caution": "注意",
+  "admonitions.changeType": "选择提示类型",
+  "admonitions.danger": "警告",
+  "admonitions.info": "信息",
+  "admonitions.note": "说明",
+  "admonitions.placeholder": "提示类型",
+  "admonitions.tip": "提示",
+  "codeBlock.inlineLanguage": "语言",
+  "codeBlock.language": "代码块语言",
+  "codeBlock.selectLanguage": "选择代码语言",
+  "codeblock.delete": "删除代码块",
+  "createLink.cancelTooltip": "取消修改",
+  "createLink.saveTooltip": "设置链接",
+  "createLink.text": "链接文字",
+  "createLink.textTooltip": "链接显示的文字",
+  "createLink.title": "链接标题",
+  "createLink.titleTooltip": "鼠标悬停时显示的标题",
+  "createLink.url": "链接地址",
+  "createLink.urlPlaceholder": "选择或粘贴链接地址",
+  "dialog.close": "关闭对话框",
+  "dialogControls.cancel": "取消",
+  "dialogControls.save": "保存",
+  "frontmatterEditor.addEntry": "添加字段",
+  "frontmatterEditor.key": "字段名",
+  "frontmatterEditor.title": "编辑文档 Frontmatter",
+  "frontmatterEditor.value": "字段值",
+  "imageEditor.deleteImage": "删除图片",
+  "imageEditor.editImage": "编辑图片",
+  "linkPreview.copied": "已复制！",
+  "linkPreview.copyToClipboard": "复制到剪贴板",
+  "linkPreview.edit": "编辑链接",
+  "linkPreview.remove": "移除链接",
+  "table.alignCenter": "居中对齐",
+  "table.alignLeft": "左对齐",
+  "table.alignRight": "右对齐",
+  "table.columnMenu": "列菜单",
+  "table.deleteColumn": "删除此列",
+  "table.deleteRow": "删除此行",
+  "table.deleteTable": "删除表格",
+  "table.insertColumnLeft": "在左侧插入列",
+  "table.insertColumnRight": "在右侧插入列",
+  "table.insertRowAbove": "在上方插入行",
+  "table.insertRowBelow": "在下方插入行",
+  "table.rowMenu": "行菜单",
+  "table.textAlignment": "文本对齐",
+  "toolbar.admonition": "插入提示块",
+  "toolbar.blockTypeSelect.placeholder": "段落类型",
+  "toolbar.blockTypeSelect.selectBlockTypeTooltip": "选择段落类型",
+  "toolbar.blockTypes.heading": "标题 {{level}}",
+  "toolbar.blockTypes.paragraph": "正文段落",
+  "toolbar.blockTypes.quote": "引用",
+  "toolbar.bold": "粗体",
+  "toolbar.bulletedList": "无序列表",
+  "toolbar.checkList": "任务列表",
+  "toolbar.codeBlock": "插入代码块",
+  "toolbar.diffMode": "差异对比",
+  "toolbar.editFrontmatter": "编辑 Frontmatter",
+  "toolbar.highlight": "高亮",
+  "toolbar.image": "插入图片",
+  "toolbar.inlineCode": "行内代码",
+  "toolbar.insertFrontmatter": "插入 Frontmatter",
+  "toolbar.italic": "斜体",
+  "toolbar.link": "创建链接",
+  "toolbar.numberedList": "有序列表",
+  "toolbar.redo": "重做 {{shortcut}}",
+  "toolbar.removeBold": "取消粗体",
+  "toolbar.removeHighlight": "取消高亮",
+  "toolbar.removeInlineCode": "取消行内代码",
+  "toolbar.removeItalic": "取消斜体",
+  "toolbar.removeStrikethrough": "取消删除线",
+  "toolbar.removeSubscript": "取消下标",
+  "toolbar.removeSuperscript": "取消上标",
+  "toolbar.removeUnderline": "取消下划线",
+  "toolbar.richText": "富文本",
+  "toolbar.source": "源码模式",
+  "toolbar.strikethrough": "删除线",
+  "toolbar.subscript": "下标",
+  "toolbar.superscript": "上标",
+  "toolbar.table": "插入表格",
+  "toolbar.thematicBreak": "插入分隔线",
+  "toolbar.toggleGroup": "切换选项",
+  "toolbar.underline": "下划线",
+  "toolbar.undo": "撤销 {{shortcut}}",
+  "uploadImage.addViaUrlInstructions": "或通过链接添加图片：",
+  "uploadImage.addViaUrlInstructionsNoUpload": "通过链接添加图片：",
+  "uploadImage.alt": "替代文本：",
+  "uploadImage.autoCompletePlaceholder": "选择或粘贴图片链接",
+  "uploadImage.dialogTitle": "上传图片",
+  "uploadImage.height": "高度：",
+  "uploadImage.title": "标题：",
+  "uploadImage.uploadInstructions": "从设备上传图片：",
+  "uploadImage.width": "宽度：",
+}
+
+const mdxEditorChineseTranslation: Translation = (key, defaultValue, interpolations) => {
+  const template = mdxEditorChineseText[key] ?? defaultValue
+  return template.replace(/\{\{(\w+)\}\}/g, (_, name: string) => String(interpolations?.[name] ?? ''))
+}
+
+export function TyporaGithubEditor({
+  markdown,
+  onChange,
+}: {
+  markdown: string
+  onChange: (markdown: string) => void
+}) {
+  return (
+    <div className="typora-github-editor">
+      <MDXEditor
+        className="genesis-mdx-editor"
+        contentEditableClassName="genesis-mdx-content"
+        markdown={markdown}
+        onChange={onChange}
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          thematicBreakPlugin(),
+          linkPlugin(),
+          linkDialogPlugin(),
+          tablePlugin(),
+          imagePlugin(),
+          codeBlockPlugin({ defaultCodeBlockLanguage: 'text' }),
+          codeMirrorPlugin({
+            codeBlockLanguages: {
+              text: '纯文本',
+              javascript: 'JavaScript',
+              typescript: 'TypeScript',
+              python: 'Python',
+              json: 'JSON',
+              bash: 'Bash',
+              css: 'CSS',
+              html: 'HTML',
+            },
+          }),
+          diffSourcePlugin({ viewMode: 'rich-text' }),
+          markdownShortcutPlugin(),
+          toolbarPlugin({
+            toolbarContents: () => (
+              <DiffSourceToggleWrapper>
+                <UndoRedo />
+                <Separator />
+                <BoldItalicUnderlineToggles />
+                <CodeToggle />
+                <Separator />
+                <BlockTypeSelect />
+                <Separator />
+                <ListsToggle options={['bullet', 'number', 'check']} />
+                <Separator />
+                <CreateLink />
+                <InsertImage />
+                <InsertTable />
+                <InsertCodeBlock />
+                <InsertThematicBreak />
+              </DiffSourceToggleWrapper>
+            ),
+          }),
+        ]}
+        spellCheck={false}
+        translation={mdxEditorChineseTranslation}
+      />
+    </div>
+  )
+}
